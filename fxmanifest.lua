1c9661fc9046d888ecbde1c0b7301268dc99275f
ianmdesign <ian@ianmlxdesign.ca>
2023-03-29 12:39:11 -0600

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+author "doxtery#3135"
+description "Free Clean LoadingScreen"
+
+files {
+    'index.html',
+    'music/song.mp3',
+    'assets/logo.png'
+}
+
+loadscreen 'index.html'
